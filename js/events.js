0c0fb8428865983aa7d23c1f72d1ee6f0cb5b6fd
Mark Bello <mcbello51286@gmail.com>
2017-11-19 18:43:25 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,36 +1,36 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert('Hey!');
+  });
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt(){
+  $('input').on('keydown', function(key){
+    if(key.which == 71 || key.which == 103){
+      alert('You pressed g');
+      return;
+    }
+  });
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.');
+  });
+}
+
 
 $(document).ready(function(){
-  function getIt(){
-    $('p').on('click', function(){
-      alert('Hey!');
-    });
-  }
-
-  function frameIt(){
-    $('img').on('load', function(){
-      $('img').addClass('tasty');
-    });
-  }
-
-  function pressIt(){
-    $('form input').on('keydown', function(key){
-      if(key.which === '71'){
-        alert('You pressed g');
-        return;
-      }
-    });
-  }
-
-  function submitIt(){
-    $('form input').on('submit', function(){
-      alert('Your form is going to be submitted now.');
-    });
-  }
-
-  function submitIt(){
-    $()
-  }
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
