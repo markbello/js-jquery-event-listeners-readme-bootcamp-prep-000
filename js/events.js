cf7e54d400678311e74772d59eff8f9c87ce8d2b
Mark Bello <mcbello51286@gmail.com>
2017-11-19 18:34:25 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function frameIt(){
 
 function pressIt(){
   $('#typing').on('keydown', function(key){
-    if(key.which === '71'){
+    if(key.which === '71' || key.which === '103'){
       alert('You pressed g');
       return;
     }
