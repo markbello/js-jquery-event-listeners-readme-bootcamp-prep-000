1f831fb2f42985d047d1fbedd677f378c8683fca
Mark Bello <mcbello51286@gmail.com>
2017-11-19 18:27:05 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
 
 $(document).ready(function(){
+  function getIt(){
+    $('p').on('click', function(){
+      alert('Hey!');
+    });
+  }
 
+  function frameIt(){
+    $('img').on('load', function(){
+      $('img').addClass('tasty');
+    });
+  }
+
+  function pressIt(){
+    $('form input').on('keydown', function(key){
+      if(key.which === '71'){
+        alert('You pressed g');
+        return;
+      }
+    });
+  }
+
+  function submitIt(){
+    $('form input').on('submit', function(){
+      alert('Your form is going to be submitted now.');
+    });
+  }
+
+  function submitIt(){
+    $()
+  }
 // call functions here
 
 });
